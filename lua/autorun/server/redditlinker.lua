00c7c905e4d73ff8ee553fd071393ff5a9873eda
VeXan39231 <parkerno552@yahoo.com>
2016-02-09 20:20:43 -0600

    Added documentation
    Added use for the script, and documented what each line does.

--- lua/autorun/server/redditlinker.lua
+++ lua/autorun/server/redditlinker.lua
@@ -16,15 +16,21 @@ http://www.steamcommunity.com/id/vexan
 --9gag is better than reddit
 
 --**********
+--USE
+--**********
+--Type '/r/' in chat, followed by a subreddit, and it will automatically change it to a link.
+--EX: '/r/globaloffensive' would automatically be set to a link that would open the subreddit.
+
+--**********
 --NOTES
 --**********
 --This is made to work for AtlasChat. If you want support for other chatboxes, add me and tell me.
 --Place the Lua file in lua/autorun/server to get it working.
 
-hook.Add( "PlayerSay", "redditlink", function( sender, str, team )
-  if ( string.sub( str, 1, 3 ) == "/r/" ) then
-    return "<url>http://www.reddit.com" .. str .. "</url>"
-  end
-end )
+hook.Add( "PlayerSay", "redditlink", function( sender, str, team ) --Add the hook that is called every time a player says something.
+  if ( string.sub( str, 1, 3 ) == "/r/" ) then --Are the first 3 letters of the message '/r/'?
+    return "<url>http://www.reddit.com" .. str .. "</url>" --If they are, change the entire message to a link that goes to the subreddit.
+  end --End the if statement.
+end ) --End the hook.
 
-MsgC( Color( 0, 255, 0 ), "VeXan's RedditLinker loaded!\n" )
+MsgC( Color( 0, 255, 0 ), "VeXan's RedditLinker loaded!\n" ) --Send the server a message that the script has loaded.
